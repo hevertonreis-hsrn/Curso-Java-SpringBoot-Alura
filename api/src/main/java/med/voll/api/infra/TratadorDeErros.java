719a61576a8949969572a0a3204acc06f53e6067
Heverton Silva Dos Reis <heverton.reis04@gmail.com>
2023-03-01 11:52:19 -0300

    Tratando Erros 400

--- api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,8 +2,12 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -11,5 +15,35 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest()
+                .body(erros.stream().map(DadosErroValidacao::new)
+                .toList());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity tratarErroSQL(SQLIntegrityConstraintViolationException ex) {
+
+        var erros = ex.getMessage();
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    private record DadosErroValidacao(
+            String campo,
+            String mensagem
+    ){
+        public DadosErroValidacao(FieldError erro){
+            this(
+                    erro.getField(),
+                    erro.getDefaultMessage()
+                    );
+        }
     }
 }
