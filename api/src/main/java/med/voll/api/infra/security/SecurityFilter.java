e8cc7237ccf5f1ef3108cef8f762a9f6bae1b0c9
Heverton Silva Dos Reis <heverton.reis04@gmail.com>
2023-03-07 17:29:16 -0300

    doc: validando token recebido

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,12 +12,17 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJWT);
 
-        System.out.println(tokenJWT);
+        System.out.println(subject);
 
         filterChain.doFilter(request, response);
     }
@@ -29,6 +35,6 @@ public class SecurityFilter extends OncePerRequestFilter {
             throw new RuntimeException("Token não enviado no cabeçalho!");
         }
 
-        return authorizationHeader;
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
